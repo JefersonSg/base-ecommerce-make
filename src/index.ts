f0fd8df6355d3f7d351c66efed7d0760b03dac58
JefersonSg <jefersongabri@gmail.com>
2024-03-16 22:37:56 -0300

    adicionaodo mais uma url ao CORS

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
     "http://localhost:3000",
     "https://base-ecommerce-git-formularios-jefersonsg.vercel.app",
     "https://basecommerce.vercel.app",
+    "https://base-ecommerce-pearl.vercel.app/",
     "https://abayomimake.com",
   ];
 
