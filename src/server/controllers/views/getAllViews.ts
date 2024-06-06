95d615baae14bcdadd963a2277a8d56662070ff3
JefersonSg <jefersongabri@gmail.com>
2024-06-05 21:39:26 -0300

    arrumando bug do new Date pegando horario do servidor

--- src/server/controllers/views/getAllViews.ts
+++ src/server/controllers/views/getAllViews.ts
@@ -1,15 +1,20 @@
 import { type Request, type Response } from "express";
 import ViewsModel from "../../db/models/Views";
 import { sub } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
 
 export const getAllViews = async (req: Request, res: Response) => {
-  const today = new Date();
+  const timeZone = "America/Sao_Paulo";
+
+  // Criar a data com o fuso horário de Brasília
+  const now = new Date();
+  const todayBrType = toZonedTime(now, timeZone);
 
   const { daysAgo } = req.params;
 
-  today.setHours(0, 0, 0, 0);
+  todayBrType.setHours(0, 0, 0, 0);
 
-  const DaysAgo = sub(today, { days: Number(daysAgo) ?? 0 });
+  const DaysAgo = sub(todayBrType, { days: Number(daysAgo) ?? 0 });
 
     try {
       const totalViews = await ViewsModel.aggregate([
