69da5923043567b3713e429a8458e556e902926a
Jeferson Gabri <jefersongabri@gmail.com>
2025-06-24 17:22:39 -0300

    arrumando o envio do email para o cliente

--- src/server/shared/helpers/emails/EmailBoasVindas.ts
+++ src/server/shared/helpers/emails/EmailBoasVindas.ts
@@ -11,7 +11,7 @@ export async function EmailBoasVindas({
 }) {
   try {
     const response = await resend.emails.send({
-      from: 'Loja Bless <contato@lojabless.com.br>',
+      from: 'Loja Bless <contato@vistabless.com>',
       to: [to],
       subject: 'Seja bem-vinda à Loja Bless!',
       html: `
@@ -20,7 +20,7 @@ export async function EmailBoasVindas({
           <p>Estamos muito felizes em ter você com a gente 💖</p>
           <p>A <strong>Loja Bless</strong> é o seu novo destino de estilo, bom gosto e elegância. A partir de agora, você receberá em primeira mão nossas novidades, promoções e tendências.</p>
           <p>Explore agora mesmo nossos lançamentos e se apaixone!</p>
-          <a href="https://lojabless.com.br" style="display:inline-block; padding:10px 20px; background:#d71920; color:#fff; border-radius:5px; text-decoration:none; font-weight:bold;">
+          <a href="https://vistabless.com" style="display:inline-block; padding:10px 20px; background:#d71920; color:#fff; border-radius:5px; text-decoration:none; font-weight:bold;">
             Ir para a Loja
           </a>
           <p style="margin-top: 20px;">Com carinho,<br>A equipe Bless 💫</p>
